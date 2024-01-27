29067ec10be3a3ba7a12c2865cb462c1aa3c591d
Krystyna Szurmanska <kszurmanska@gmail.com>
2024-01-27 11:56:13 -0500

    update image route

--- rent-increase-calculator/src/components/CarouselItem.js
+++ rent-increase-calculator/src/components/CarouselItem.js
@@ -9,7 +9,7 @@ export const CarouselItem = ({  texts, index }) => {
 
     return (
       <div className="carousel-item flex flex-col	justify-center	" >
-        <div className = "my-7 carousel-image flex justify-center" >   <img src={process.env.PUBLIC_URL + imagePath} alt="" /> </div>
+        <div className = "my-7 carousel-image flex justify-center" >   <img src={process.env.PUBLIC_URL + "/" + imagePath} alt="" /> </div>
         <div className = "carousel-text" dangerouslySetInnerHTML={{__html: t(`${texts}.modalTexts`,  { returnObjects: true })[index]}} />
       </div>
     );
